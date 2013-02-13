1a9f793fe1ffcf96da7842d93e677de0b7c8e400
Adrian Aiken <goatmancer@gmail.com>
2013-02-13 10:45:33 -0500

    Image asset updates
    Bumped up quality and made menu items actually make sense

--- assets.lua
+++ assets.lua
@@ -8,21 +8,21 @@ GameAssets	= group{}
 
 -- backgrounds
 Background	= image{"shake.png", quality=9.5}
-PressBackground = image{"press.png", quality=5}
-FlipBackground = image{"Flip.png", quality=5}
-StopBackground = image{"Stop.png", quality=5}
-Blank		= image{"middlebg.png", quality=3}
-MiddleBG    = image{"tug_of_war_centercube.png", quality=3}
-DoneBack	= image{"Checkmark.png", quality=5}
+PressBackground = image{"press.png", quality=9.5}
+FlipBackground = image{"Flip.png", quality=9.5}
+StopBackground = image{"Stop.png", quality=9.5}
+Blank		= image{"middlebg.png", quality=9.5}
+MiddleBG    = image{"tug_of_war_centercube.png", quality=9.5}
+DoneBack	= image{"Checkmark.png", quality=9.5}
 Winner		= image{"Winner.png", quality=9.5, width=128, height=128}
-PlayerImage = image{"Player.png", quality=5, width=128, height=128}
-TeamImage	= image{"Team.png", quality=5, width=128, height=128}
+PlayerImage = image{"Player.png", quality=9.5, width=128, height=128}
+TeamImage	= image{"Team.png", quality=9.5, width=128, height=128}
 
 -- game images
-Knot		= image{"knot_tog.png", pinned=true, width=64, height=64, quality=7}
-Font		= image{"font-8x16.png", pinned=true, width=8, height=16}
-Ball		= image{"ball.png", pinned=true, width=16, height=16}
-Digits		= image{"digits.png", pinned=true, width=8, height=16}
+Knot		= image{"knot_tog.png", pinned=true, width=64, height=64, quality=9.7}
+Font		= image{"font-8x16.png", pinned=true, width=8, height=16, quality=9.7}
+Ball		= image{"ball.png", pinned=true, width=16, height=16, quality=9.7}
+Digits		= image{"digits.png", pinned=true, width=8, height=16, quality=9.7}
 Fullprog	= image{"prog_full.png"}
 Halfprog	= image{"prog_half.png"}
 Noneprog	= image{"prog_none.png"}
@@ -31,10 +31,10 @@ Noneprog	= image{"prog_none.png"}
 BgTile = image{ "bg.png", pinned=1 }
 StripeTile = image{ "stripes.png", pinned=1 }
 
-IconTwoPlayer = image{ "IconSandwich.png" }
-IconFourPlayer = image{ "IconChroma.png" }
-IconSixPlayer = image{ "IconPeano.png" }
-IconEightPlayer = image{ "IconBuddy.png"}
+IconTwoPlayer = image{ "2_player.png" }
+IconFourPlayer = image{ "4_player.png" }
+IconSixPlayer = image{ "6_player.png" }
+IconEightPlayer = image{ "8_player.png"}
 
 Tip0 = image{ "Tip0.png" }
 Tip1 = image{ "Tip1.png" }
